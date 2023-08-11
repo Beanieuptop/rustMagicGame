bcbccc3a79195ff35c204b2042716e5b11f1890f
Beanieuptop <Beanieuptop@users.noreply.github.com>
2023-08-10 22:35:31 -0700

    Local Reorganization

---
+++ target/debug/deps/RustMG-07f2e48258abf584.d
@@ -0,0 +1,5 @@
+/home/beanieuptop/Desktop/RustDevelopment/rustMagicGame/target/debug/deps/RustMG-07f2e48258abf584.rmeta: src/main.rs
+
+/home/beanieuptop/Desktop/RustDevelopment/rustMagicGame/target/debug/deps/RustMG-07f2e48258abf584.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RustMG-0d07ad8f3244b6d9.d
@@ -0,0 +1,5 @@
+/home/beanieuptop/Desktop/RustDevelopment/rustMagicGame/target/debug/deps/RustMG-0d07ad8f3244b6d9.rmeta: src/main.rs
+
+/home/beanieuptop/Desktop/RustDevelopment/rustMagicGame/target/debug/deps/RustMG-0d07ad8f3244b6d9.d: src/main.rs
+
+src/main.rs:
